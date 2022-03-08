161e9285f5e8e93308a746b8f35a3f3831ae2632
BiasedLiar <Bretagard12345>
2022-03-08 12:38:27 +0100

    Last commit tuesday

--- dockerproject/hello-docker/hello.py
+++ dockerproject/hello-docker/hello.py
@@ -1,3 +1,6 @@
-print('hei')
-spam = 2
-print(spam*5)
+spam = 0
+
+for i in range(10):
+    print('This is the ' + str(i) + " time through the loop.")
+    spam += i
+    print('Spam: ' + str(spam))
