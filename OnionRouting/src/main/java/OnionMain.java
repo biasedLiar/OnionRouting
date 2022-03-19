1c2c40b5652fc23f788edc733c6e470baa0b4e7d
BiasedLiar <Bretagard12345>
2022-03-19 20:34:29 +0100

    Basic direct communication added

--- OnionRouting/src/main/java/OnionMain.java
+++ OnionRouting/src/main/java/OnionMain.java
@@ -1,5 +1,21 @@
-public class OnionMain {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class OnionMain {
+    public static void main(String[] args) throws IOException {
+        System.out.println("HEllo world");
+        Scanner in = new Scanner(System.in);
+        try {
+            new OnionServer().start();
+            OnionClient client = new OnionClient();
+            client.runCalculator();
+            client.close();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 }
